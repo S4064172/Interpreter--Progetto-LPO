8613385ad5b470be2f19ef5131d25c1a0d97096d
S4064172 <32641379+S4064172@users.noreply.github.com>
2017-11-23 00:18:46 +0100

    ->added some tests to check parseTimesOrDiv() ->small fix --> renamed some test funtion (cut and past is evil!)

--- Interpreter/src/testUnit/ParsetTest/JUParsetTest.java
+++ Interpreter/src/testUnit/ParsetTest/JUParsetTest.java
@@ -274,4 +274,89 @@ public class JUParsetTest {
 		} 
 		
 	}
+	
+	
+	@Test
+	public void testTimesOrDivRight()
+	{
+		ArrayList<String> relustList = new ArrayList<String>();
+		try(Scanner s = new Scanner(new File("src/testUnit/ParsetTest/testTimesOrDivResult.txt")))
+		{
+			while (s.hasNext())
+			{
+				relustList.add(s.next());
+			}
+		s.close();
+		}catch (FileNotFoundException e) {
+			fail(e.getMessage());
+		} catch (Throwable e) {
+			fail("Unexpected error. " + e.getMessage());
+		}
+		int i = 0;
+		String result=null;
+		try(Tokenizer t = new StreamTokenizer(new FileReader("src/testUnit/ParsetTest/testTimesOrDivRight.txt") ))
+		{
+			while (t.hasNext()) 
+			{
+				StreamParser p = new StreamParser(t);
+				Method method = p.getClass().getDeclaredMethod("parseTimesOrDiv", null);
+				method.setAccessible(true);
+				t.next();
+				try
+				{
+					result= method.invoke(p).toString();
+					assertTrue(result.equals(relustList.get(i)));
+					i++;
+				}catch (Exception e) {
+					fail(e.getCause().getMessage());
+				} 
+			}
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		} 
+		
+	}
+	
+	@Test
+	public void testTimesOrDivWrong()
+	{
+		try(Tokenizer t = new StreamTokenizer(new FileReader("src/testUnit/ParsetTest/testTimesOrDivWrong.txt") ))
+		{
+			while (t.hasNext()) 
+			{
+				StreamParser p = new StreamParser(t);
+				Method method = p.getClass().getDeclaredMethod("parseTimesOrDiv", null);
+				method.setAccessible(true);
+				t.next();
+				try
+				{
+					String res =method.invoke(p).toString();
+					fail("correst -->"+res);
+				}catch (Exception e) 
+				{
+				
+					if(	e.getCause().getClass().equals(ParserException.class) ||
+						e.getCause().getClass().equals(ScannerException.class) ||
+						e.getCause().getClass().equals(IOException.class))
+					{
+						while(!t.tokenString().equals(";") && t.hasNext())
+						{
+							t.next();
+						}
+					}
+					else
+						fail("found "+e.getCause().getClass()+" expeted "+ParserException.class+" OR" 
+								+ScannerException.class+"OR"
+								+ IOException.class);
+					
+				} 
+			}
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		} 
+		
+	}
+	
 }
