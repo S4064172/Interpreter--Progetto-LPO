a40a5a7791afedb0f998ab077673aa4d2ee1f6e2
S4064172 <32641379+S4064172@users.noreply.github.com>
2017-11-23 00:07:16 +0100

    ->added some tests to check Eval() for operation AddOrSub

--- Interpreter/src/testUnit/EvalTest/JUEvalTest.java
+++ Interpreter/src/testUnit/EvalTest/JUEvalTest.java
@@ -18,12 +18,14 @@ import _2_StreamParser.ParserException;
 import _2_StreamParser.StreamParser;
 import _2_Tokenizer.StreamTokenizer;
 import _2_Tokenizer.Tokenizer;
+import _3_Ast.Add;
 import _3_Ast.ConCat;
 import _3_Ast.Fst;
 import _3_Ast.IntLiteral;
 import _3_Ast.Length;
 import _3_Ast.Pair;
 import _3_Ast.Snd;
+import _3_Ast.Sub;
 import _4_Visitors.evaluation.Eval;
 import _4_Visitors.typechecking.TypeCheck;
 import _4_Visitors.typechecking.TypecheckerException;
@@ -130,6 +132,61 @@ public class JUEvalTest {
 			
 				pp.accept(new TypeCheck());
 				result =pp.accept(new Eval()).toString();
+				
+				try
+				{
+					assertTrue(result.equals(relustList.get(i)));
+				}catch(Throwable e)
+				{
+					fail("found "+ result + " expeted "+relustList.get(i));
+				}
+				i++;
+			}
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		} 
+	}
+
+	@Test
+	public void TestAddOrSubCheckTypeRight() 
+	{
+		
+		ArrayList<String> relustList = new ArrayList<String>();
+		try(Scanner s = new Scanner(new File("src/testUnit/EvalTest/TestAddOrSubEvalResult.txt")))
+		{
+			while (s.hasNext())
+			{
+				relustList.add(s.nextLine());
+			}
+		s.close();
+		}catch (FileNotFoundException e) {
+			fail(e.getMessage());
+		} catch (Throwable e) {
+			fail("Unexpected error. " + e.getMessage());
+		}
+		int i = 0;
+		
+		try(Tokenizer t = new StreamTokenizer(new FileReader("src/testUnit/EvalTest/TestAddOrSubEvalRight.txt") ))
+		{
+			while (t.hasNext()) 
+			{
+				StreamParser p = new StreamParser(t);
+				Method method = p.getClass().getDeclaredMethod("parseAddOrSub", null);
+				method.setAccessible(true);
+				t.next();
+				String result = null;
+				Object pp =  method.invoke(p);
+				if(pp instanceof Sub)
+				{
+					((Sub)pp).accept(new TypeCheck()).toString();
+					result =((Sub)pp).accept(new Eval()).toString();
+				}
+				else
+				{
+					((Add)pp).accept(new TypeCheck()).toString();
+					result =((Add)pp).accept(new Eval()).toString();
+				}
 				
 				try
 				{
